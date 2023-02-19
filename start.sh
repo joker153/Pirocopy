2f7201147bad73bfb0e1d40a2bb8b4c27a3b75a9
joker153 <77005994+joker153@users.noreply.github.com>
2023-02-19 23:02:56 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/joker153/PiroAutoFilterBot.git /PiroAutoFilterBot
+  git clone https://github.com/joker153/Pirocopy.git /Pirocopy
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /PiroAutoFilterBot
+  git clone $UPSTREAM_REPO /Pirocopy
 fi
 cd /PiroAutoFilterBot
 pip3 install -U -r requirements.txt
-echo "Starting PiroAutoFilterBot...."
+echo "Starting Tovino...."
 python3 bot.py
