b65b211bd2f4d759dbe8329ee953e4122aa98ec4
joker153 <77005994+joker153@users.noreply.github.com>
2023-02-19 22:54:58 +0530

    Add files via upload

--- plugins/commands.py
+++ plugins/commands.py
@@ -150,7 +150,7 @@ async def start(client, message):
                     file_id=msg.get("file_id"),
                     caption=f_caption,
                     protect_content=msg.get('protect', False),
-                    reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('⚔️ 𝖯𝖨𝖱𝖮 𝖴𝖯𝖣𝖠𝖳𝖤𝖲 ⚔️', url="https://t.me/rai_info17") ] ] ),
+                    reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('⚔️ ᴄɪɴᴇꜰʟɪx  ⚔️', url="https://t.me/{SUPPORT_CHAT}") ] ] ),
                     
                 )
             except FloodWait as e:
@@ -161,7 +161,7 @@ async def start(client, message):
                     file_id=msg.get("file_id"),
                     caption=f_caption,
                     protect_content=msg.get('protect', False),
-                    reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('⚔️ 𝖯𝖨𝖱𝖮 𝖴𝖯𝖣𝖠𝖳𝖤𝖲 ⚔️', url="https://t.me/rai_info17") ] ] ),
+                    reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('⚔️ ᴄɪɴᴇꜰʟɪx  ⚔️', url="https://t.me/{SUPPORT_CHAT}") ] ] ),
                 )
             except Exception as e:
                 logger.warning(e, exc_info=True)
@@ -223,7 +223,7 @@ async def start(client, message):
                 chat_id=message.from_user.id,
                 file_id=file_id,
                 protect_content=True if pre == 'filep' else False,
-                reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('⚔️ 𝖯𝖨𝖱𝖮 𝖴𝖯𝖣𝖠𝖳𝖤𝖲 ⚔️', url="https://t.me/rai_info17") ] ] ),
+                reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('⚔️ ᴄɪɴᴇꜰʟɪx  ⚔️', url="https://t.me/{SUPPORT_CHAT}") ] ] ),
             )
             filetype = msg.media
             file = getattr(msg, filetype.value)
@@ -257,7 +257,7 @@ async def start(client, message):
         file_id=file_id,
         caption=f_caption,
         protect_content=True if pre == 'filep' else False,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('⚔️ 𝖯𝖨𝖱𝖮 𝖴𝖯𝖣𝖠𝖳𝖤𝖲 ⚔️', url="https://t.me/rai_info17") ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('⚔️ ᴄɪɴᴇꜰʟɪx  ⚔️', url="https://t.me/{SUPPORT_CHAT}") ] ] ),
     )
                     
 
